0530f0d9640b31c2f0166cf627163701020c1c8f
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-03-13 13:49:59 +0100

    adding header navigate button

--- App.js
+++ App.js
@@ -1,26 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import AllPlaces from './screens/AllPlaces';
-import AddPlace from './screens/AddPlace';
+import AllPlaces from "./screens/AllPlaces";
+import AddPlace from "./screens/AddPlace";
+import IconButton from "./UI/IconButton";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-    <StatusBar/>
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="All Places" component={AllPlaces}></Stack.Screen>
-        <Stack.Screen name="Add Place" component={AddPlace}></Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+      <StatusBar />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={({navigation}) => ({
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  icon="add"
+                  size={24}
+                  color={tintColor}
+                  onPress={() => navigation.navigate("AddPlace")}
+                />
+              ),
+            })}
+          />
+          <Stack.Screen name="AddPlace" component={AddPlace}></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
