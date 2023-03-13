ca29a8e9ba435d1652ce2817722e88fdf18b2c82
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-03-13 17:36:22 +0100

    fetching users location

--- components/LocationPicker.js
+++ components/LocationPicker.js
@@ -1,9 +1,42 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Alert } from "react-native";
 import OutlinedButton from "../UI/OutlinedButton";
-import {Colors} from "../constants/colors";
+import { Colors } from "../constants/colors";
+import {
+  getCurrentPositionAsync,
+  useForegroundPermissions,
+  PermissionStatus,
+} from "expo-location";
 
 function LocationPicker() {
-  function getLocationHandler() {}
+  const [locationPermissionInformation, requestPermission] =
+    useForegroundPermissions();
+
+  async function verifyPermissions() {
+    if (
+      locationPermissionInformation.status === PermissionStatus.UNDETERMINED
+    ) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient permissions!",
+        "You need to grant location permissions to use this app"
+      );
+      return false;
+    }
+    return true;
+  }
+
+  async function getLocationHandler() {
+    const hasPermission = await verifyPermissions();
+    if (!hasPermission) {
+        return;
+    }
+
+    const location = await getCurrentPositionAsync();
+    console.log(location);
+  }
   function pickOnMapHandler() {}
 
   return (
@@ -24,18 +57,18 @@ function LocationPicker() {
 export default LocationPicker;
 
 const styles = StyleSheet.create({
-    mapPreview: {
-        width: "100%",
-        height: 200,
-        marginVertical: 8,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: Colors.primary100,
-        borderRadius: 4,
-    },
-    actions: {
-        flexDirection: 'row',
-        justifyContent: "space-around",
-        alignItems: "center"
-    }
+  mapPreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  actions: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
 });
