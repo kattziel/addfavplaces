3d8c1eed99392b8352baa2abb210f7576b1e2558
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-03-14 11:58:12 +0100

    allowing map interactions

--- screens/Map.js
+++ screens/Map.js
@@ -1,20 +1,45 @@
+import { useState } from "react";
 import { StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 
 function Map() {
-    const region = {
-        latitude: 37.78,
-        longitude: -122.43,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421
-    };
+  const [selectedLocation, setSelectedLocation] = useState();
+  const region = {
+    latitude: 37.78,
+    longitude: -122.43,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
 
-  return <MapView style={styles.map} initialRegion={region}></MapView>;
+  function selectLocationHandler(event) {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+
+    setSelectedLocation({ lat: lat, lng: lng });
+  }
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
+  );
 }
 
 export default Map;
 
 const styles = StyleSheet.create({
-    map: {
-    }
+  map: {
+    flex: 1
+  },
 });
--- util/location.js
+++ util/location.js
@@ -1,6 +1,6 @@
-const GOOGLE_API_KEY = 'AIzaSyD3fIyojEJAs9QrQM61K4AeXNDqCgQJaNU';
+const GOOGLE_API_KEY = "AIzaSyCCN3maEDjRP-sUKYk3vJOyxAcQTY-iIKs";
 
 export function getMapPreview(lat, lng) {
-    const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
-    return imagePreviewUrl;
-  }
+  const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:C%7${lat},${lng}&key=${GOOGLE_API_KEY}&signature=YOUR_SIGNATURE`;
+  return imagePreviewUrl;
+}
