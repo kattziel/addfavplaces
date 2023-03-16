3e765a5d25a7d0c4cf6591945b6752722747a65f
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-03-16 13:30:41 +0100

    styling the list

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -16,6 +16,7 @@ function PlacesList({ places }) {
 
   return (
     <FlatList
+    style={styles.list}
       data={places}
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => <PlaceItem place={item} />}
@@ -37,4 +38,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: Colors.primary200
   },
+  list: {
+    margin: 24
+  }
 });
